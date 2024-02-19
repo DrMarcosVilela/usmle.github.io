3ae5da6c6f41e98a9286ddf6e8984ec738891c85
DrMarcosVilela <129998448+DrMarcosVilela@users.noreply.github.com>
2024-02-19 18:03:30 -0300

    script.js

---
+++ css/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    // DOM is fully loaded, but maybe waiting on images & stylesheets
+    // Initial script setup or event listeners can go here
+});
